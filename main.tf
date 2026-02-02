eb44949f414f2b29458bf3411f2b3bfe14bff534
Mitali Paul <paulmitali16@gmail.com>
2026-02-02 11:39:53 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "basic_server" {
+  ami           = "ami-0c101f26f147fa7fd" # Amazon Linux 2023
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Basic-GitOps-Server"
+  }
+}
